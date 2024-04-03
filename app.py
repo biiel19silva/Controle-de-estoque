86b79cc5385f151c57cb5e0cc8291c2818d90d25
Guilherme Leao <guilhermecarmo18@gmail.com>
2024-04-03 20:10:40 -0300

    movements.html

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def index():
 def produtos():
     return render_template('products.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/localidades')
+def localidades():
+    return render_template('locations.html')
+
+@app.route('/movimentacoes')
+def movimentacoes():
+    return render_template('movements.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
